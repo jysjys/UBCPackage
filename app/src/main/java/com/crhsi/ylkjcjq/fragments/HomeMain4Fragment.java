f33ebb01fd09b11efbd52d19968567fb4524dbc1
ylkjcjq <691424873@qq.com>
2017-11-18 15:51:46 +0800

    fragment01

---
+++ app/src/main/java/com/crhsi/ylkjcjq/fragments/HomeMain4Fragment.java
@@ -0,0 +1,40 @@
+package com.crhsi.ylkjcjq.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.crhsi.ylkjcjq.R;
+import com.crhsi.ylkjcjq.activitys.AboutUsActivity;
+import com.crhsi.ylkjcjq.utils.LoginConfig;
+
+
+/**
+ *
+ * @author CJQ
+ */
+public class HomeMain4Fragment extends Fragment implements View.OnClickListener{
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main4, null);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.aboutus:
+                startActivity(new Intent(getContext(), AboutUsActivity.class));
+                break;
+            default:
+
+                break;
+        }
+    }
+}
